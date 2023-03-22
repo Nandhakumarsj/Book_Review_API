89b4bf296c2192279e9730c8471bc05a29eb5b7c
Nandhakumarsj <sjnk0206123@outlook.com>
2023-03-22 18:34:41 +0530

    Asynchronous methods are now used for general user

--- router/general.js
+++ router/general.js
@@ -5,11 +5,11 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 public_users.post("/register", (req, res) => {
-  const username = req.body['username'];
-  const password = req.body['password'];
+  const username = req.body["username"];
+  const password = req.body["password"];
   if (username && password) {
     if (isValid(username)) {
-      users.push({'username':username, 'password':password});
+      users.push({ username: username, password: password });
       return res.send("User registered successfully");
     } else {
       return res.send("User name is already taken");
@@ -19,15 +19,17 @@ public_users.post("/register", (req, res) => {
   }
 });
 
-// Get the book list available in the shop
-public_users.get("/", function (req, res) {
-  return res.status(200).send(JSON.stringify(books, null, 4));
+// Get the book list available in the
+public_users.get("/", async function (req, res) {
+  const titles = await new Promise((resolve, reject) => resolve(Object.values(books).map((book) => book['title']))
+  );
+  return res.send(JSON.stringify(titles, null, 4)); 
 });
 
 // Get book details based on ISBN
-public_users.get("/isbn/:isbn", function (req, res) {
+public_users.get("/isbn/:isbn", async function (req, res) {
   const isbn = req.params.isbn;
-  let collected_books = books[Number(isbn)];
+  let collected_books = await new Promise(resolve=> books[Number(isbn)])
   if (collected_books) {
     return res.send(JSON.stringify(collected_books, null, 4));
   } else {
@@ -36,12 +38,14 @@ public_users.get("/isbn/:isbn", function (req, res) {
 });
 
 // Get book details based on author
-public_users.get("/author/:author", function (req, res) {
+public_users.get("/author/:author",async function (req, res) {
   const author = req.params.author;
-  const book_array = Object.entries(books);
-  const author_books = book_array.filter(
-    (book) => book[1]["author"] === author
-  );
+  const author_books = await new Promise((resolve, reject)=>{
+    const book_array = Object.entries(books);
+    resolve(book_array.filter(
+      (book) => book[1]["author"] === author
+    ));
+  });
   if (author_books.length > 0) {
     return res.send(JSON.stringify(author_books, null, 4));
   }
@@ -49,10 +53,12 @@ public_users.get("/author/:author", function (req, res) {
 });
 
 // Get all books based on title
-public_users.get("/title/:title", function (req, res) {
+public_users.get("/title/:title", async function (req, res) {
   const title = req.params.title;
-  const book_array = Object.entries(books);
-  const book_names = book_array.filter((book) => book[1]["title"] === title);
+  const book_names = await new Promise((resolve, reject)=>{
+    const book_array = Object.entries(books);
+    resolve(book_array.filter((book) => book[1]["title"] === title));
+  });
   if (book_names.length > 0) {
     return res.send(JSON.stringify(book_names, null, 4));
   }
@@ -64,7 +70,7 @@ public_users.get("/review/:isbn", function (req, res) {
   const isbn = req.params.isbn;
   const book = books[isbn];
   let reviews = book["reviews"];
-  if (Object.entries(reviews).length>0) {
+  if (Object.entries(reviews).length > 0) {
     return res.status(200).send(JSON.stringify(reviews, null, 4));
   }
   return res.send("No Review Recorded");
