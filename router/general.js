b0979ae4a317f858d49bae1c0b1cf5decd176316
Nandhakumarsj <sjnk0206123@outlook.com>
2023-03-22 11:14:53 +0530

    Authorized user access granted.

--- router/general.js
+++ router/general.js
@@ -6,26 +6,30 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(200).send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  let collected_books = books[Number(isbn)];
+  if (collected_books){
+    return res.send(collected_books);
+  }
+  else{
+    return res.status(404).json({message: 'Book not Found'});
+}
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  let written_books = {...books}
 });
 
 // Get all books based on title
